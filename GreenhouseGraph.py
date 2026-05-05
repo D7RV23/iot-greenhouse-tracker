f8ab47d9973a835a1dcf27071dcc7b7c443ad544
Anthony DeMatteo <anthde13@gmail.com>
2026-05-05 00:17:57 -0400

    X-axis should always be time, humidity is measured in RH percentages.

--- GreenhouseGraph.py
+++ GreenhouseGraph.py
@@ -6,27 +6,27 @@ def create_greenhouse_graph(data_rows):
     temps = [r[0] for r in data_rows]
     hums = [r[1] for r in data_rows]
     
-    # Temperature
-    x = np.array([(i / 60) for i in range(len(temps))])
+    # Time array in hours (0-24 hour period)
+    time = np.array([i for i in range(len(temps))])
 
     fig = plt.figure(figsize=(8, 6))
     
     plt.subplot(2, 1, 1) # Top plot
-    plt.plot(x, temps)
+    plt.plot(time, temps)
     plt.title("Temperature")
     plt.xlim((0, 24)) # min, max
-    plt.ylim((-20, 50)) # min, max
-    plt.xlabel("Celsius")
-    plt.ylabel("Time (hr)")
+    plt.ylim((10, 30)) # min, max
+    plt.xlabel("Time (hr)")
+    plt.ylabel("Temperature (°C)")
 
     # Humidity:
     plt.subplot(2, 1, 2) # Bottom plot
-    plt.plot(x, hums)
+    plt.plot(time, hums)
     plt.title("Humidity")
     plt.xlim((0, 24)) # min, max
     plt.ylim((0, 100)) # min, max
-    plt.xlabel("Celsius")
-    plt.ylabel("Time (hr)")
+    plt.xlabel("Time (hr)")
+    plt.ylabel("Relative Humidity (%)")
 
     plt.tight_layout()
     return fig # removed plt.show()
